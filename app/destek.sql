5ea3bd10752efd1c9e17e24bd27835f7bacd77b1
pinar <taspnar@gmail.com>
2015-11-10 14:49:13 +0200

    -- Kullanıcı bilgileri eklendi

--- app/destek.sql
+++ app/destek.sql
@@ -1,3 +1,28 @@
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `surname` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `salt` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `enabled` tinyint(1) NOT NULL DEFAULT '0',
+  `role` text,
+  `deleted` tinyint(1) NOT NULL DEFAULT '0',
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Tablo döküm verisi `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `surname`, `username`, `email`, `salt`, `password`, `enabled`, `role`, `deleted`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'ADMİN', 'ADMİN', NULL, 'admin@admin.com', 'g9ro51ar0w00c00cswco0s4cgokc8gg', 'FyPAfqjdK6E6mlfpBvD2PacwXMu1NqCJB8sYl6ZSR3IZfDdVYQMipFkboUqN/oGgrpq1EdLQi9uLI78tY/mjjg==', 1, 'ROLE_ADMIN', 0, '2015-11-07 12:19:58', NULL, NULL),
+(2, 'USER', 'USER', NULL, 'user@user.com', 'lvlizmxr1z4wccs0ooko0sk8k80k8gw', 'TeFX/w9Z7FQeghKBRimlUeR7H3zDLk6BDuB1TvAR8fncZ9682hBuKUu3IhdxhsoTMZB+uuxBfgPjEi8xQ/INeA==', 1, 'ROLE_USER', 0, '2015-11-07 12:19:58', NULL, NULL);
+
 CREATE TABLE category (
 id INT AUTO_INCREMENT NOT NULL,
 user_id INT NOT NULL,
